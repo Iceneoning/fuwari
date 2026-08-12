fc2d3d9d084dfc1bef69a6ae51943189757f83e6
Iceneoning <woshisos@foxmail.com>
2026-08-12 18:02:13 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://iceneoning-blog.pages.dev/",
 	base: "/",
 	trailingSlash: "always",
 	compressHTML: true,
